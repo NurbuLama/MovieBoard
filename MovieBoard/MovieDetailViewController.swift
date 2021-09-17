0995aea46eabd8aa63880ebb15be7ecbc4593482
NurbuLama <nurbu.lama02@qmail.cuny.edu>
2021-09-17 15:12:05 -0400

    part 2

---
+++ MovieBoard/MovieDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MovieDetailViewController.swift
+//  MovieBoard
+//
+//  Created by Nurbu Angdu on 9/14/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterVIew: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+  
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        posterVIew.af_setImage(withURL: posterUrl!)
+
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdopURL = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdopURL!)
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MovieBoard/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  MovieBoard
+//
+//  Created by Nurbu Angdu on 9/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
